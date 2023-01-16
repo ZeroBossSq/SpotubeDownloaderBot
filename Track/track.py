c48b8aedcf7858d6e6dce90d7ec44c43c63002b3
ZeroBossSq <82669735+ZeroBossSq@users.noreply.github.com>
2023-01-16 23:12:13 +0200

    Some updates

--- Track/track.py
+++ Track/track.py
@@ -20,19 +20,19 @@ class SpotiTrack:
         alb_imgs = [i.get('url') for i in track.get('album').get('images')]
 
         self.name = track.get('name')
-        self.album_imgs = SpotifyTrackImages(url_small=alb_imgs[0],
+        self.album_imgs = SpotifyTrackImages(url_small=alb_imgs[2],
                                              url_medium=alb_imgs[1],
-                                             url_large=alb_imgs[2])
+                                             url_large=alb_imgs[0])
         self.artists = [i.get('name') for i in track.get('artists')]
         self.duration = round(float(track["duration_ms"]) / 1000 / 60, 1)  # round to 1 num after .
         self.full_name = f'{self.name} - {", ".join(self.artists)}'
 
 
 class SpotifyTrackImages:
-    def __init__(self, url_small: str, url_medium: str, url_large: str):
-        self.small = SpotifyTrackImage(url=url_small)
-        self.medium = SpotifyTrackImage(url=url_medium)
+    def __init__(self, url_large: str, url_medium: str, url_small: str):
         self.large = SpotifyTrackImage(url=url_large)
+        self.medium = SpotifyTrackImage(url=url_medium)
+        self.small = SpotifyTrackImage(url=url_small)
 
 
 class SpotifyTrackImage:
--- main.py
+++ main.py
@@ -1,60 +1,108 @@
 from string import ascii_letters
-from os import remove
+from os import remove, getenv
 
 import pytube
 import telebot
 
 from Track.track import SpotiClient
 
-bot = telebot.TeleBot('5796112493:AAFlNo1Zy63DDa1PilW3nUwu8kNDm7-4vdk')
+bot = telebot.TeleBot(getenv('spotify_downloader_bot_token'))
 
-s_client = SpotiClient(client_id='114f74f9e39642088f38dd2ff019c55b',
-                       client_secret='fef5e90b9bb743a392e109a7d0fc5934')
+s_client = SpotiClient(client_id=getenv('spotify_downloader_bot_client_id'),
+                       client_secret=getenv('spotify_downloader_bot_client_secret'))
 
 
 def justify_track_fname(track_full_name: str):
     return ''.join([i for i in list(track_full_name) if i in ascii_letters])
 
 
+@bot.message_handler(commands=['start', 'help'])
+def start(msg: telebot.types.Message):
+    markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+    markup.add(
+        telebot.types.InlineKeyboardButton('Donate', callback_data='donate'),
+        telebot.types.InlineKeyboardButton('Support', url='https://t.me/ZXBTLKXC')
+    )
+
+    bot.send_message(msg.chat.id, "Hi! 👋\n"
+                                  "I'm a bot 🤖 downloads music from spotify 🎧 and youtube 🎬 that's under 5️⃣ minutes long!\n"
+                                  "Just send me link to the track and I'll download it for you! ✨", reply_markup=markup)
+
+
 @bot.message_handler()
 def msg_handler(msg: telebot.types.Message):
-    track_name = ''
-    founded = False
+    try:
+        track_name = ''
+        founded = False
 
-    if 'spotify' in msg.text:
-        track = s_client.get_track(msg.text)
+        if 'spotify' in msg.text:
+            bot.send_message(msg.chat.id, '⏳')
 
-        # bot.send_message(msg.chat.id, '')
-        bot.send_photo(msg.chat.id, photo=track.album_imgs.medium.bytes,
-                       caption=f'`{track.full_name}` ({track.duration} min) (Images: '
-                               f'[Large]({track.album_imgs.large.url}), [Medium]({track.album_imgs.medium.url}), [Small]({track.album_imgs.small.url}))',
-                       parse_mode='MARKDOWN')
+            track = s_client.get_track(msg.text)
 
-        track_name = track.full_name
-        founded = True
-    elif 'youtu' in msg.text:
-        track = pytube.YouTube(msg.text)
-        track_name = f'{track.title} - {track.author}'
+            bot.send_photo(msg.chat.id, photo=track.album_imgs.medium.bytes,
+                           caption=f'`{track.full_name}` ({track.duration} min) (Images: '
+                                   f'[Large]({track.album_imgs.large.url}), [Medium]({track.album_imgs.medium.url}), [Small]({track.album_imgs.small.url}))',
+                           parse_mode='MARKDOWN')
 
-        bot.send_photo(msg.chat.id, track.thumbnail_url, track_name)
-        founded = True
+            track_name = track.full_name
+            founded = True
+        elif 'youtu' in msg.text:
+            bot.send_message(msg.chat.id, '⏳')
 
-    if founded:
-        message = bot.send_message(msg.chat.id, text='Searching')
-        download_track: pytube.YouTube = pytube.Search(track_name).results[0]
+            track = pytube.YouTube(msg.text)
+            track_name = f'{track.title} - {track.author}'
 
-        bot.edit_message_text('Downloading', msg.chat.id, message.id)
-        download_track.streams.get_audio_only().download(filename=f'{justify_track_fname(track_name)}.mp3')
+            bot.send_photo(msg.chat.id, track.thumbnail_url, track_name)
+            founded = True
 
-        bot.edit_message_text('Sending', msg.chat.id, message.id)
-        track_bytes = open(f'{justify_track_fname(track_name)}.mp3', 'rb').read()
+        if founded:
+            # Trying to find song in the logs
+            message = bot.send_message(msg.chat.id, text='Searching 🔍')
+            download_track: pytube.YouTube = pytube.Search(track_name).results[0]
 
-        bot.send_audio(msg.chat.id, audio=track_bytes, title=track_name)
-        bot.edit_message_text('Done', msg.chat.id, message.id)
+            if download_track.length <= 320:
+                bot.edit_message_text('Downloading 🔰', msg.chat.id, message.id)
+                download_track.streams.get_audio_only().download(filename=f'{justify_track_fname(track_name)}.mp3')
 
-        remove(justify_track_fname(track_name))
-    else:
-        bot.send_message(msg.chat.id, text="Your search query doesn't found")
+                bot.edit_message_text('Sending ⚙️', msg.chat.id, message.id)
+                track_bytes = open(f'{justify_track_fname(track_name)}.mp3', 'rb').read()
+
+                bot.send_audio(msg.chat.id, audio=track_bytes, title=track_name)
+                bot.edit_message_text('Done ✅', msg.chat.id, message.id)
+
+                remove(f'{justify_track_fname(track_name)}.mp3')
+            else:
+                bot.edit_message_text('The length of the song you requested is > 5 minutes! 😖', msg.chat.id, message.id)
+        else:
+            bot.send_message(msg.chat.id, text="Your search query doesn't found! 😞\n"
+                                               "Send me a spotify track link 🎧 || a youtube video link! 💾 (length ≤ 5 minutes)")
+    except BaseException:
+        bot.send_message(msg.chat.id, text='An error has occurred! 🪫\n'
+                                           'Video download stopped! ❌')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(callback: telebot.types.CallbackQuery):
+    match callback.data:
+        case 'donate':
+            markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+            markup.add(
+                telebot.types.InlineKeyboardButton('Back ↩️', callback_data='main_menu'),
+            )
+
+            bot.edit_message_text('Come back here later! 🎃', callback.from_user.id, callback.message.id, reply_markup=markup)
+        case 'main_menu':
+            markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+            markup.add(
+                telebot.types.InlineKeyboardButton('Donate 💎', callback_data='donate'),
+                telebot.types.InlineKeyboardButton('Support 🧰', url='https://t.me/ZXBTLKXC')
+            )
+
+            bot.edit_message_text("Hi! 👋\n"
+                                  "I'm a bot 🤖 downloads music from spotify 🎧 and youtube 🎬 that's under 5️⃣ minutes long!\n"
+                                  "Just send me link to the track and I'll download it for you! ✨",
+                                  callback.from_user.id, callback.message.id, reply_markup=markup)
 
 
 print(f'@{bot.get_me().username} | {bot.get_me().full_name} [{bot.get_me().id}]')
